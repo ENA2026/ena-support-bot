218d3c2447133483d5186068d5aa7fbeb1a2979c
ENA2026 <ENA2026@github.com>
2026-04-02 12:12:17 -0300

    Add home page

---
+++ pages/index.js
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div style={{ padding: '40px', textAlign: 'center' }}>
+      <h1>ENA Support Bot</h1>
+      <p>Aplicacion para analizar correos</p>
+      <a href="/api/auth/login">Iniciar Sesion</a>
+    </div>
+  );
+}
